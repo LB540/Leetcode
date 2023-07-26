d52d62e9171e5bdaf73bfd3e16096c66f43f5ec7
Lalit Bisht <70473998+LB540@users.noreply.github.com>
2023-07-27 02:52:27 +0530

    Time: 307 ms (77.46%), Space: 101.3 MB (94.22%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     int minSpeedOnTime(vector<int>& dist, double hour) {
-        int l=1, r=10e7+5;
-        int min_speed=INT_MAX;
+        int l=1,r=10e7+5;
+        int min_speed = INT_MAX;
         while(l<r) {
-            int mid = l+(r-l)/2;
-            double total_time = 0;
+            int mid = l + (r-l)/2;
+            double t_time = 0;
             for(auto d: dist) {
-                if(total_time > (int)total_time) {
-                    total_time = (double)((int)total_time+1);
-                }
-                total_time += ((double)(d))/((double)(mid));
+                if((double)t_time>(int)t_time) t_time =(int)t_time+1;
+                t_time = t_time + (double)d/(double)mid;
             }
-            if(total_time > hour) l = mid+1;
-            else { 
-                r = mid;
+            if(t_time>hour) {
+                l=mid+1;
+            }else {
+                r=mid;
                 min_speed = mid;
             }
         }
-        return (l>10000000 ? -1:min_speed);
+        if(l>10e7) {
+            return -1;
+        }
+        return min_speed;
     }
 };
