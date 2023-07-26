b6f695dcfa90c9711f7147ec305bf025bdcdbc65
Lalit Bisht <70473998+LB540@users.noreply.github.com>
2023-07-27 03:50:12 +0530

    Time: 40 ms (98.12%), Space: 14.5 MB (55.72%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.cpp
@@ -9,35 +9,48 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1 = headA;
-        ListNode* temp2 = headB;
-        int size1=0;
-        int size2=0;
-        while(temp1!=NULL){
-            temp1 = temp1->next;
-            size1++;
-        }
-        while(temp2!=NULL){
-            temp2 = temp2->next;
-            size2++;
-        }
-        int size_t;
-        if(size1>size2) {
-            size_t = size1-size2;
-            while(size_t--) {
-                headA=headA->next;
+        // ListNode* temp1 = headA;
+        // ListNode* temp2 = headB;
+        // int size1=0;
+        // int size2=0;
+        // while(temp1!=NULL){
+        //     temp1 = temp1->next;
+        //     size1++;
+        // }
+        // while(temp2!=NULL){
+        //     temp2 = temp2->next;
+        //     size2++;
+        // }
+        // int size_t;
+        // if(size1>size2) {
+        //     size_t = size1-size2;
+        //     while(size_t--) {
+        //         headA=headA->next;
+        //     }
+        // }else {
+        //     size_t = size2-size1;
+        //     while(size_t--) {
+        //         headB=headB->next;
+        //     }
+        // }
+        // while(headA!=NULL && headB!=NULL && headA != headB) {
+        //     headA = headA->next;
+        //     headB = headB->next;
+        // }
+        // if(headA ==NULL || headB == NULL) return NULL;
+        // return headA;
+        
+        ListNode* a = headA, *b = headB;
+        while(a!=b) {
+            if(a!=NULL) {
+                a=a->next;
+            }else {
+                a=headB;
             }
-        }else {
-            size_t = size2-size1;
-            while(size_t--) {
-                headB=headB->next;
-            }
+            
+            if(b!=NULL) b=b->next;
+            else b = headA;
         }
-        while(headA!=NULL && headB!=NULL && headA != headB) {
-            headA = headA->next;
-            headB = headB->next;
-        }
-        if(headA ==NULL || headB == NULL) return NULL;
-        return headA;
+        return a;
     }
 };
