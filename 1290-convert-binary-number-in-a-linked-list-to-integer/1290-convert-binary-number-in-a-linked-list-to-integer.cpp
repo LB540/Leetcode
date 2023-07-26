d8fe4263bfe23d321adc4e0d9bbd7741a8f27530
Lalit Bisht <70473998+LB540@users.noreply.github.com>
2023-07-26 20:55:59 +0530

    Time: 0 ms (100.00%), Space: 8.3 MB (6.39%) - LeetHub

--- 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
+++ 1290-convert-binary-number-in-a-linked-list-to-integer/1290-convert-binary-number-in-a-linked-list-to-integer.cpp
@@ -18,21 +18,21 @@ public:
             binary+=c;
             head = head->next;
         }
-        reverse(binary.begin(),binary.end());
+        // reverse(binary.begin(),binary.end());
         // int n =stoi(binary);
         int res=0;
         int n =binary.length();
         
-        for(int i=0;i<n;i++) {
-            if(binary[i] == '1') {
-                res+=pow(2,i);
-            }
-        }
-        // for(int i=n-1;i>=0;i--) {
-        //     if(binary[i]=='1') {
-        //         res+= pow(2,n-i+1);
+        // for(int i=0;i<n;i++) {
+        //     if(binary[i] == '1') {
+        //         res+=pow(2,i);
         //     }
         // }
+        for(int i=n-1;i>=0;i--) {
+            if(binary[i]=='1') {
+                res+= pow(2,n-i-1);
+            }
+        }
         return res;
     }
 };
